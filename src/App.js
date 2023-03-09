91932ca31d01bbadd6dca0159dd6c1a5191191a3
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-09 21:09:34 +0530

    created the background ellipse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from "react"
+import Ellipse from "./components/Ellipse";
 
 
 function App() {
   return (
-    <div></div>
+    <React.Fragment>
+      <Ellipse/>
+      {/* <Users/>
+      <UserInfo/>
+      <Main_User/>
+      <DownButton1/>
+      <DownButton2/> */}
+    </React.Fragment>
   );
 }
 
